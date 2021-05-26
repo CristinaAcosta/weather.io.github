2781d5c66e1794f4e2f2730cd32b95176c6a42b4
Cristina Acosta <cacost5695@yahoo.com>
2021-05-25 22:31:16 -0700

    script

--- script.js
+++ script.js
@@ -1,51 +1,15 @@
+var APIKEY = a7d5b1be6d4e6c114b7127c477b641fe;
+var requestUrl = ('https://api.openweathermap.org/data/2.5/forecast?q={city name},{state code},{country code}&appid={a7d5b1be6d4e6c114b7127c477b641fe}');
+var city = $("");
+var date = $("");
+var temp = $("temp");
+var air = $("air");
+var water = $("water");
+var fire = $("fire");
 
-
-
-
-
+// Calling the search function
 $(document).ready(function() {
-    $("#form-sub").submit(function(event) { 
-        performSearch(event); });
-  });
-
-  function performSearch(event) {
-    var request;
-    event.preventDefault();
-    $("#city-name").text("Searching ...");
-    $("#city-temp").text("");
-    $("#city-weather").text("");
-  
-    // Send the request
-    request = $.ajax({
-        url: 'api.openweathermap.org/data/2.5/weather?q={city name}&appid={a7d5b1be6d4e6c114b7127c477b641fe}',
-        type: "GET",
-        data: { q: $("#city").val(), 
-                appid: 'a7d5b1be6d4e6c114b7127c477b641fe', 
-                units: 'metric'}
-    });
-
-  
-    // Callback handler for success
-    request.done(function (response){
-        formatSearchResults(response);
-    });
-    
-    // Callback handler for failure
-    request.fail(function (){
-        $("#city-name").text("try again");
-        $("#city-temp").text("");
-        $("#city-weather").text("");
-    });
-  
-  }
-    
-  function formatSearchResults(jsonObject) {
-    
-    var city_name = jsonObject.name;
-    city_name = city_name + ", " + jsonObject.sys.country;
-    var city_weather = jsonObject.weather[0].main;
-    var city_temp = jsonObject.main.temp;
-    $("#city-name").text(city_name);
-    $("#city-weather").text(city_weather);
-    $("#city-temp").text(city_temp+" Celsius");  
-  }
+  $("#search").on("click", function() {
+    var searchResults = $('#search').value();
+    console.log(('#search').value());
+  }}
