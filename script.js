4d42d766d05d7ec1e2939d1b1bc887109cfeb178
Cristina Acosta <cacost5695@yahoo.com>
2021-05-25 22:44:57 -0700

    scripttwo'

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var APIKEY = a7d5b1be6d4e6c114b7127c477b641fe;
+var APIKEY = "a7d5b1be6d4e6c114b7127c477b641fe";
 var requestUrl = ('https://api.openweathermap.org/data/2.5/forecast?q={city name},{state code},{country code}&appid={a7d5b1be6d4e6c114b7127c477b641fe}');
 var city = $("");
 var date = $("");
@@ -12,4 +12,15 @@ $(document).ready(function() {
   $("#search").on("click", function() {
     var searchResults = $('#search').value();
     console.log(('#search').value());
-  }}
+  }};
+
+  function weather(city) {
+    var APIKEY = '{a7d5b1be6d4e6c114b7127c477b641fe}';
+    fetch('https://tile.openweathermap.org/map/{layer}/{z}/{x}/{y}.png?' + city+ '&appid=' + APIKEY)  
+    .then(function(resp) { 
+      return resp.json() 
+    })
+    .then(function(data) {
+    })
+    .catch(function() {
+    });
